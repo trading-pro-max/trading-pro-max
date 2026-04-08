b598ae392f89a03c06d1c1cd423f894301f99f1b
Trading Pro Max <ahmadalahmad10041993@gmail.com>
2026-04-08 23:19:38 +0200

    tpm: infinity jump

--- scripts/tpm-infinity-loop.mjs
+++ scripts/tpm-infinity-loop.mjs
@@ -1,3 +1,6 @@
 ﻿import { runInfinityCycle } from "../lib/tpm-infinity-core.mjs";
 function sleep(ms){ return new Promise(r=>setTimeout(r,ms)); }
-while(true){ runInfinityCycle(); await sleep(90000); }
+while(true){
+  runInfinityCycle();
+  await sleep(90000);
+}
