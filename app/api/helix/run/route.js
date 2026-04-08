f67b9f2f533abd61f7f694130fd4b08c5ef63f5e
Trading Pro Max <ahmadalahmad10041993@gmail.com>
2026-04-08 13:40:34 +0200

    tpm: helix jump

---
+++ app/api/helix/run/route.js
@@ -0,0 +1,8 @@
+﻿import { NextResponse } from "next/server";
+import { runHelixCycle } from "../../../../lib/tpm-helix-core.mjs";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(){
+  return NextResponse.json(runHelixCycle());
+}
