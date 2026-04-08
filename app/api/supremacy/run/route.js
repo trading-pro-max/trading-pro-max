758b350fa0768797edf020ffdf77b956296277f0
Trading Pro Max <ahmadalahmad10041993@gmail.com>
2026-04-09 01:40:10 +0200

    tpm: supremacy mega jump

---
+++ app/api/supremacy/run/route.js
@@ -0,0 +1,8 @@
+﻿import { NextResponse } from "next/server";
+import { runSupremacyCycle } from "../../../../lib/tpm-supremacy-core.mjs";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(){
+  return NextResponse.json(runSupremacyCycle());
+}
---
+++ scripts/tpm-supremacy-loop.mjs
@@ -0,0 +1,6 @@
+﻿import { runSupremacyCycle } from "../lib/tpm-supremacy-core.mjs";
+function sleep(ms){ return new Promise(r=>setTimeout(r,ms)); }
+while(true){
+  runSupremacyCycle();
+  await sleep(90000);
+}
