92d7428609f1b8c3d4aa923c4efceaf00f5a76f8
Trading Pro Max <ahmadalahmad10041993@gmail.com>
2026-04-08 12:58:35 +0200

    tpm: omega jump

---
+++ scripts/tpm-omega-loop.mjs
@@ -0,0 +1,6 @@
+﻿import { runOmegaCycle } from "../lib/tpm-omega-core.mjs";
+function sleep(ms){ return new Promise(r=>setTimeout(r,ms)); }
+while(true){
+  runOmegaCycle();
+  await sleep(90000);
+}
